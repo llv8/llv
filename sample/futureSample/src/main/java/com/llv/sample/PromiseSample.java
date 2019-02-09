d7e867942dbc8d2fa791281b8ef5bf73b6176af0
Lewis <787365902@qq.com>
2019-02-09 23:31:19 +0800

    java promise

--- sample/futureSample/src/main/java/com/llv/sample/PromiseSample.java
+++ sample/futureSample/src/main/java/com/llv/sample/PromiseSample.java
@@ -1,13 +1,21 @@
 package com.llv.sample;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 public class PromiseSample {
 	public static void main(String[] args) throws InterruptedException {
 		// completedFutureExample();
 		// runAsyncExample();
 		// thenApplyExample();
-		thenApplyAsyncExample();
+		// thenApplyAsyncExample();
+		// thenApplyAsyncWithExecutorExample();
+		// thenAcceptExample();
+		thenAcceptAsyncExample();
 	}
 
 	static void completedFutureExample() {
@@ -35,6 +43,12 @@ public class PromiseSample {
 	static void thenApplyExample() {
 		CompletableFuture cf = CompletableFuture.completedFuture("message").thenApply(s -> {
 			System.out.println(Thread.currentThread() + "--isDeamon:" + Thread.currentThread().isDaemon());
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			return s.toUpperCase();
 		});
 		System.out.println(cf.getNow(null));
@@ -53,6 +67,56 @@ public class PromiseSample {
 		});
 		System.out.println(cf.getNow(null));
 		System.out.println(cf.join());
+	}
+
+	static ExecutorService executor = Executors.newFixedThreadPool(3, new ThreadFactory() {
+		int count = 1;
+
+		@Override
+		public Thread newThread(Runnable runnable) {
+			return new Thread(runnable, "custom-executor-" + count++);
+		}
+	});
+
+	static void thenApplyAsyncWithExecutorExample() {
+		CompletableFuture cf = CompletableFuture.completedFuture("message").thenApplyAsync(s -> {
+			System.out.println(Thread.currentThread().getName());
+			System.out.println(Thread.currentThread().isDaemon());
+
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			return s.toUpperCase();
+		}, executor);
+		System.out.println(cf.getNow(null));
+		System.out.println(cf.join());
+	}
+
+	static void thenAcceptExample() {
+		StringBuilder result = new StringBuilder();
+		CompletableFuture.completedFuture("thenAccept message").thenAccept(s -> {
+			System.out.println(Thread.currentThread().getName());
+			result.append(s.toUpperCase());
+		});
+		System.out.println(result);
+	}
+
+	static void thenAcceptAsyncExample() {
+		StringBuilder result = new StringBuilder();
+		CompletableFuture cf = CompletableFuture.completedFuture("thenAcceptAsync message").thenAcceptAsync(s -> {
+			System.out.println(Thread.currentThread().getName());
+			result.append(s);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		cf.join();
+		System.out.println(result);
 	}
 
 }
